da567ef5bd74a05ad014bd649092d3f7b8656e95
Victor Cisneiros <victorcisneiros@gmail.com>
2025-04-24 20:33:51 -0300

    Update docker.ts

--- src/model/docker.ts
+++ src/model/docker.ts
@@ -117,7 +117,7 @@ class Docker {
             --volume "${actionFolder}/BlankProject":"c:/BlankProject" \
             --cpus=${dockerCpuLimit} \
             --memory=${dockerMemoryLimit} \
-            --device class/5B45201D-F2F2-4F3B-85BB-30FF1F953599 \
+            --device="class/5B45201D-F2F2-4F3B-85BB-30FF1F953599" \
             --isolation=process \
             ${image} \
             powershell c:/steps/entrypoint.ps1`;
